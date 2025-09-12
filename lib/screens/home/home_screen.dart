f39c505060aa2b6cfc4360847b4c79660863de7c
TalhaSalman318 <talhaalvi318@gmail.com>
2025-09-12 12:48:13 +0500

    ..

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:book_hunt/screens/work_detail/work_details_screen.dart';
+import 'package:book_hunt/widgets/color.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:book_hunt/providers/search_provider.dart';
@@ -9,74 +10,67 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final provider = Provider.of<SearchBooksProvider>(context);
-    final controller = TextEditingController();
+    final searchController = TextEditingController();
+    final width = MediaQuery.of(context).size.width;
 
     return Scaffold(
-      appBar: AppBar(title: const Text("Book Hunt")),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          children: [
-            // Search box
-            Row(
+      appBar: AppBar(title: const Text('Book Hunt')),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            child: TextField(
+              controller: searchController,
+              decoration: InputDecoration(
+                filled: true,
+                fillColor: AppColors.searchGrey,
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(20),
+                  borderSide: BorderSide.none,
+                ),
+                hintText: 'Search books...',
+                suffixIcon: IconButton(
+                  icon: const Icon(Icons.search),
+                  onPressed: () {
+                    provider.search(searchController.text);
+                  },
+                ),
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 20),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
-                Expanded(
-                  child: TextField(
-                    controller: controller,
-                    decoration: const InputDecoration(
-                      hintText: "Search books...",
-                      border: OutlineInputBorder(),
-                    ),
+                Text(
+                  "Most Popular",
+                  style: TextStyle(
+                    fontSize: width * 0.04,
+                    fontWeight: FontWeight.bold,
+                    color: AppColors.blackColor,
                   ),
                 ),
-                const SizedBox(width: 8),
-                ElevatedButton(
-                  onPressed: () {
-                    provider.search(controller.text);
-                  },
-                  child: const Text("Search"),
+                Row(
+                  children: [
+                    Text(
+                      "More",
+                      style: TextStyle(
+                        color: AppColors.fontGreyColor,
+                        fontSize: width * 0.035,
+                      ),
+                    ),
+                    Icon(
+                      Icons.arrow_forward_ios,
+                      size: width * 0.04,
+                      color: AppColors.fontGreyColor,
+                    ),
+                  ],
                 ),
               ],
             ),
-            const SizedBox(height: 16),
-
-            // Results
-            Expanded(
-              child: provider.isLoading
-                  ? const Center(child: CircularProgressIndicator())
-                  : ListView.builder(
-                      itemCount: provider.results.length,
-                      itemBuilder: (context, index) {
-                        final book = provider.results[index];
-                        final title = book['title'] ?? "Untitled";
-                        final author =
-                            (book['author_name'] != null &&
-                                book['author_name'].isNotEmpty)
-                            ? book['author_name'][0]
-                            : "Unknown";
-                        final workId = (book['key'] as String).replaceFirst(
-                          "/works/",
-                          "",
-                        );
-
-                        return ListTile(
-                          title: Text(title),
-                          subtitle: Text(author),
-                          onTap: () {
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                builder: (_) =>
-                                    WorkDetailScreen(workId: workId),
-                              ),
-                            );
-                          },
-                        );
-                      },
-                    ),
-            ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
---
+++ lib/widgets/color.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color mainColor = Color(0xFFB1732E);
+  static const Color secondaryColor = Color(0xFFF6AB27);
+  static const Color blackColor = Color(0xFF000000);
+  static const Color highlightColor = Color(0xFFFFD5A6);
+  static const Color fontGreyColor = Color(0xFF999797);
+  static const Color navBarGray = Color(0xFF919191);
+  static const Color searchGrey = Color(0xFFF2F2F2);
+  static const Color whiteColor = Color(0xFFFFFFFF);
+}
